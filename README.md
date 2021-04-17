6ce5476d55939c1d04e094d685806b5fb29718ca
Tiago Lustosa Santos <tiagolustosasantos@hotmail.com>
2021-04-17 08:06:29 -0300

    Shop App with state management(Cod3r course)

---
+++ README.md
@@ -0,0 +1,10 @@
+# Shop App
+
+Shop App(Cod3r Course)
+
+## App gifs examples
+
+![App Sample](assets/images/Shop.gif)
+![App Sample](assets/images/Shop_2.gif)
+
+
---
+++ lib/views/products_overview_screen.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/providers/cart.dart';
+import 'package:shop/utils/app_routes.dart';
+import 'package:shop/widgets/app_drawer.dart';
+import 'package:shop/widgets/badge.dart';
+import 'package:shop/widgets/product_grid.dart';
+
+enum FilterOptions { Favorite, All }
+
+class ProductOverviwScreen extends StatefulWidget {
+  @override
+  _ProductOverviwScreenState createState() => _ProductOverviwScreenState();
+}
+
+class _ProductOverviwScreenState extends State<ProductOverviwScreen> {
+  bool _showFavoriteOnly = false;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(
+          child: Text('Minha Loja'),
+        ),
+        actions: [
+          PopupMenuButton(
+            onSelected: (FilterOptions selectedValue) {
+              setState(() {
+                if (selectedValue == FilterOptions.Favorite) {
+                  _showFavoriteOnly = true;
+                } else {
+                  _showFavoriteOnly = false;
+                }
+              });
+            },
+            icon: Icon(Icons.more_vert),
+            itemBuilder: (_) => [
+              PopupMenuItem(
+                child: Text('Somente Favoritos'),
+                value: FilterOptions.Favorite,
+              ),
+              PopupMenuItem(
+                child: Text('Todos'),
+                value: FilterOptions.All,
+              ),
+            ],
+          ),
+          Consumer<Cart>(
+            child: IconButton(
+              icon: Icon(Icons.shopping_cart),
+              onPressed: () {
+                Navigator.of(context).pushNamed(AppRoutes.CART);
+              },
+            ),
+            builder: (_, cart, child) => Badge(
+              value: cart.itemsCount.toString(),
+              child: child,
+            ),
+          ),
+        ],
+      ),
+      body: ProductGrid(_showFavoriteOnly),
+      drawer: AppDrawer(),
+    );
+  }
+}
